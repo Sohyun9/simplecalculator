3747dab862d0a64b67ea5990391f219893ec7096
Sohyun9 <lsm0908@naver.com>
2022-02-15 18:27:51 +0900

    02-15

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello Calc");
+    res.send("URL 계산기 서비스 <br> 덧셈 : /add , 뺄셈 : /minus, 곱셈 : /multi, 나눗셈 : /div");
 });
 
 app.get("/add", (req, res) => {
@@ -10,8 +10,7 @@ app.get("/add", (req, res) => {
     var p2 = req.param('p2');
     var result = (Number)(p1) + (Number)(p2);
 
-    // res.send("Param1 : " + p1 + " / Param2 : " + p2);
-    res.send("Result : " + result);
+    res.send("덧셈" + "<br>" + "Param1 : " + p1 + " / Param2 : " + p2 + "<br>" +"Result : " + result);
 });
 
 app.get("/minus", (req, res) => {
@@ -19,8 +18,7 @@ app.get("/minus", (req, res) => {
     var p2 = req.param('p2');
     var result = (Number)(p1) - (Number)(p2);
 
-    // res.send("Param1 : " + p1 + " / Param2 : " + p2);
-    res.send("Result : " + result);
+    res.send("뺄셈" + "<br>" + "Param1 : " + p1 + " / Param2 : " + p2 + "<br>" +"Result : " + result);
 });
 
 app.get("/multi", (req, res) => {
@@ -28,8 +26,7 @@ app.get("/multi", (req, res) => {
     var p2 = req.param('p2');
     var result = (Number)(p1) * (Number)(p2);
 
-    // res.send("Param1 : " + p1 + " / Param2 : " + p2);
-    res.send("Result : " + result);
+    res.send("곱셈" + "<br>" + "Param1 : " + p1 + " / Param2 : " + p2 + "<br>" +"Result : " + result);
 });
 
 app.get("/div", (req, res) => {
@@ -37,8 +34,11 @@ app.get("/div", (req, res) => {
     var p2 = req.param('p2');
     var result = (Number)(p1) / (Number)(p2);
 
-    // res.send("Param1 : " + p1 + " / Param2 : " + p2);
-    res.send("Result : " + result);
+    if(0 == (Number)(p2)) {
+        res.send("0으로 수를 나눌 수 없습니다.")
+    }
+
+    res.send("나눗셈" + "<br>" + "Param1 : " + p1 + " / Param2 : " + p2 + "<br>" +"Result : " + result);
 });
 
 app.listen(8888);
